ae5de895ddbd9380466e7e9edfdb54d865bafac3
Martin Civáň <martin.civan5@gmail.com>
2024-02-23 11:50:32 +0100

    Add Stimulus

--- app/importmap.php
+++ app/importmap.php
@@ -28,4 +28,10 @@ return [
         'version' => '5.3.3',
         'type' => 'css',
     ],
+    '@hotwired/stimulus' => [
+        'version' => '3.2.2',
+    ],
+    '@symfony/stimulus-bundle' => [
+        'path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js',
+    ],
 ];
